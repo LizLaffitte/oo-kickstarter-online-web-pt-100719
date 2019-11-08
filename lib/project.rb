a2c2e8cacce7c0d1cefcd99dbe9bd87869fd10f3
Liz Laffitte <liz.kossakowski@gmail.com>
2019-11-08 19:46:02 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,19 +1,14 @@
 class Project
   attr_reader :title, :backers
-  @@all = []
   
   def initialize(title)
     @title = title
     @backers = []
-    @@all << self
   end
   
   def add_backer(backer_obj)
     @backers << backer_obj
   end
   
-  def self.all
-    @@all
-  end
   
 end
